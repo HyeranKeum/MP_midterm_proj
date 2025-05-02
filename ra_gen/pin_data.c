9af538bc5c51ebf49282a466fbe311d962acd23f
Hyeran Keum <156819180+HyeranKeum@users.noreply.github.com>
2025-05-02 09:45:07 +0900

    Fnd 모듈 확인 (#3)
    * gitignore
    
    * FND 모듈 동작 확인

--- ra_gen/pin_data.c
+++ ra_gen/pin_data.c
@@ -20,6 +20,14 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
                   | (uint32_t) IOPORT_PERIPHERAL_DEBUG) },
           { .pin = BSP_IO_PORT_03_PIN_00, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
                   | (uint32_t) IOPORT_PERIPHERAL_DEBUG) },
+          { .pin = BSP_IO_PORT_03_PIN_05, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW) },
+          { .pin = BSP_IO_PORT_03_PIN_06, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW) },
+          { .pin = BSP_IO_PORT_03_PIN_07, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW) },
+          { .pin = BSP_IO_PORT_03_PIN_08, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW) },
           { .pin = BSP_IO_PORT_04_PIN_03, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
                   | (uint32_t) IOPORT_PERIPHERAL_GPT1) },
           { .pin = BSP_IO_PORT_04_PIN_10, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
@@ -28,6 +36,22 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
                   | (uint32_t) IOPORT_PERIPHERAL_SCI0_2_4_6_8) },
           { .pin = BSP_IO_PORT_04_PIN_15, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
                   | (uint32_t) IOPORT_PERIPHERAL_GPT1) },
+          { .pin = BSP_IO_PORT_06_PIN_04, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+          { .pin = BSP_IO_PORT_06_PIN_05, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+          { .pin = BSP_IO_PORT_06_PIN_06, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+          { .pin = BSP_IO_PORT_06_PIN_07, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+          { .pin = BSP_IO_PORT_06_PIN_11, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+          { .pin = BSP_IO_PORT_06_PIN_12, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+          { .pin = BSP_IO_PORT_06_PIN_13, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+          { .pin = BSP_IO_PORT_06_PIN_14, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                  | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
           { .pin = BSP_IO_PORT_09_PIN_00, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
                   | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW) },
           { .pin = BSP_IO_PORT_09_PIN_01, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
